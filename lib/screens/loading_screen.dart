6c9fa7a3a1d1f0822d63f038cb7d0940bbc7b566
tomonori_ueno <t.uepon@gmail.com>
2023-01-21 21:48:18 +0900

    Create location.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,24 +7,40 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
-    LocationPermission permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
+
+// @override
+// Widget build(BuildContext context) {
+//   String myMargin = '15';
+//   double myMarginAsADouble;
+//
+//   try {
+//     myMarginAsADouble = double.parse(myMargin);
+//   } catch (e) {
+//     print(e);
+//   }
+//
+//   return Scaffold(
+//     body: Container(
+//       margin: EdgeInsets.all(myMarginAsADouble ?? 30.0),
+//       color: Colors.red,
+//     ),
+//   );
+// }
---
+++ lib/services/location.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  // Location({@required this.latitude, @required this.longitude});
+
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
+
+// void getLocation() async {
+//   try {
+//     Position position = await Geolocator.getCurrentPosition(
+//         desiredAccuracy: LocationAccuracy.low);
+//     print(position);
+//   } catch (e) {
+//     print(e);
+//   }
+// }
